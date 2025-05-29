1b8d5d86a2ea7c472bccee987f50d5ab00472b62
cemresirin <cemre25355@gmail.com>
2025-05-29 17:31:01 +0300

    Polymorphism başarıyla tamamlandı

---
+++ src/PolymorphismExample.java
@@ -0,0 +1,26 @@
+public class PolymorphismExample {
+    public static void main(String[] args) {
+        Animal cat = new Cat() ;  //polymorphism örnek
+        Animal dog = new Dog() ;
+
+        cat.makeSound() ;
+        dog.makeSound() ;
+    }
+}
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+class Cat extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Meow!");
+    }
+}
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Woof!");
+    }
+}
