d22cbcb78453e013b0d50aa49bffbe9abb362e95
cemresirin <cemre25355@gmail.com>
2025-05-29 16:07:02 +0300

    Method Overriding uygulandı

---
+++ src/Car4.java
@@ -0,0 +1,18 @@
+class Vehiclee {
+    void start() {
+        System.out.println("Araç çalıştırılıyor " )  ;
+    }
+}
+class CarX extends Vehiclee {
+@Override
+void start() {
+System.out.println("Araç harekete hazır. " ) ;
+}
+}
+
+public class Car4 {
+    public static void main(String[] args) {
+CarX myCar=new CarX() ;
+myCar.start();
+}
+}
