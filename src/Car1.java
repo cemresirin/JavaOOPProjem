e505347565a7bb5fab42901b2a566d0d5ee44101
cemresirin <cemre25355@gmail.com>
2025-05-29 12:38:54 +0300

    Constructor örneği yapıldı

---
+++ src/Car1.java
@@ -0,0 +1,17 @@
+public class Car1 {
+    String marka;
+    String model;
+    int yil;
+
+    //constructor
+    Car1(String marka, String model, int yil) {
+        this.marka = marka;
+        this.model = model;
+        this.yil = yil;
+    }
+
+    public static void main(String[] args) {
+        Car1 araba = new Car1("Renault", "Clio", 2025);
+        System.out.println(araba.marka + " " + araba.model + " " + araba.yil);
+    }
+    }
