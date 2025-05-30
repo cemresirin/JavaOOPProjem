04d6e82f1e229f5dd1eebc5ccb48554caa419776
cemresirin <cemre25355@gmail.com>
2025-05-30 15:18:46 +0300

    Car sınıfı oluşturuldu.

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
+class Car {
+
+    String marka;
+    String model;
+    int yil;
+}
 public class Main {
     public static void main(String[] args) {
 
-        Car araba = new Car("Renault","Clio", 2025 );
+        Car araba = new Car();
+        araba.marka = "Renault";
+        araba.model = "Clio";
+        araba.yil = 2025;
 
         System.out.println("Marka: " + araba.marka);
         System.out.println("Model: " + araba.model);
         System.out.println("Yıl: " + araba.yil);
     }
-    }
- class Car {
-
-    String marka;
-    String model;
-    int yil;
-
-    Car(String marka, String model, int yil) {
-        this.marka = marka;
-        this.model = model;
-        this.yil = yil;
-    }
-    }
+}
